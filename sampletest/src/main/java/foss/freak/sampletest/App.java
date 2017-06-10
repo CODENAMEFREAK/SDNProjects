d68d6dd14f3b04d8bb86434a447f7c8277916d22
CODENAMEFREAK <anubhav.nitsri.it@gmail.com>
2017-06-10 16:25:25 +0530

    Floodlight Test  Commit

--- sampletest/src/main/java/foss/freak/sampletest/App.java
+++ sampletest/src/main/java/foss/freak/sampletest/App.java
@@ -1,5 +1,7 @@
 package foss.freak.sampletest;
 
+import foss.freak.topology.FreakTopologyTesterEngine;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        FreakTopologyTesterEngine.main(args);
     }
 }
--- sampletest/src/main/java/foss/freak/topology/FreakTopologyTesterEngine.java
+++ sampletest/src/main/java/foss/freak/topology/FreakTopologyTesterEngine.java
@@ -1,10 +1,28 @@
 package foss.freak.topology;
 
-public class FreakTopologyTesterEngine {
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+public class FreakTopologyTesterEngine {
+	private static String ip = "10.112.19.131";
+	private static String port = "6654";
+	private static String prefix="http://"+ip+":"+port; 
 	public static void main(String[] args) {
 		
+			makeCallGetSwitchDPID();
+	}
 
+	private static void makeCallGetSwitchDPID() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target(prefix+"/wm/core/controller/switches/json");
+		
+		Response response = target.request(MediaType.APPLICATION_JSON_TYPE).get();
+		System.out.println(response);
+		
+		
 	}
 
 }
